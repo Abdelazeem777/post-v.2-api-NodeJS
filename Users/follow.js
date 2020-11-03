dd8c01815586024c8d0577b8f94b51e4e3efebae
Abdelazeem Kuratem <abdelazeem263@gmail.com>
2020-11-03 15:58:16 +0200

    add unFollow feature

--- Users/follow.js
+++ Users/follow.js
@@ -32,4 +32,36 @@ function addCurrentUserIDtoTargetUserFollowersList(User, response, currentUserID
         }
     });
 }
-module.exports = follow;
+function unFollow(request, response, User) {
+    currentUserID = request.body.currentUserID;
+    targetUserID = request.body.targetUserID;
+
+    removeTargetIDToCurrentUserFollowingList(User, response, currentUserID, targetUserID);
+    removeCurrentUserIDtoTargetUserFollowersList(User, response, currentUserID, targetUserID);
+
+    return response.send('Ok');
+
+}
+
+function removeTargetIDToCurrentUserFollowingList(User, response, currentUserID, targetUserID) {
+    const query = { '_id': ObjectID(currentUserID) };
+    const options = { $pull: { followingRankedList: targetUserID } };
+    User.updateMany(query, options, (error, _result) => {
+        if (error) {
+            console.error("follow: " + error);
+            return response.status(500).send(error);
+        }
+
+    });
+}
+function removeCurrentUserIDtoTargetUserFollowersList(User, response, currentUserID, targetUserID) {
+    const query = { '_id': ObjectID(targetUserID) };
+    const options = { $pull: { followersList: currentUserID } };
+    User.updateMany(query, options, (error, _result) => {
+        if (error) {
+            console.error("follow: " + error);
+            return response.status(500).send(error);
+        }
+    });
+}
+module.exports = { follow, unFollow };
--- server.js
+++ server.js
@@ -4,14 +4,15 @@ const alternateLogin = require('./Account/alternate_login.js');
 const updateProfilePic = require('./Account/update_profile_pic.js');
 const updateProfileData = require('./Account/update_profile_data.js');
 const deleteAccount = require('./Account/delete_account.js');
-const express = require("express");
-const BodyParser = require("body-parser");
+const express = require('express');
+const BodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 var path = require('path');
 const socketConnection = require('./socket.js');
 const searchForUser = require('./Users/searchForUser.js');
-const follow = require('./Users/follow.js');
+const { response } = require('express');
+const { follow, unFollow } = require('./Users/follow.js');
 var publicDir = path.join(__dirname, 'usersProfilePictures');
 
 
@@ -27,8 +28,8 @@ http.listen(3000, () => {
     MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true, useNewUrlParser: true }, (err, client) => {
         assert.strictEqual(null, err);
         const db = client.db('postDB');
-        User = db.collection("user");
-        console.log("Connected successfully to server");
+        User = db.collection('user');
+        console.log('Connected successfully to server');
 
 
         //client.close();
@@ -36,16 +37,17 @@ http.listen(3000, () => {
 });
 
 //account routes
-app.post("/account/signup", (request, response) => signup(request, response, User));
-app.post("/account/login", (request, response) => login(request, response, User));
-app.post("/account/alternateLogin", (request, response) => alternateLogin(request, response, User));
+app.post('/account/signup', (request, response) => signup(request, response, User));
+app.post('/account/login', (request, response) => login(request, response, User));
+app.post('/account/alternateLogin', (request, response) => alternateLogin(request, response, User));
 app.delete('/account/deleteAccount/:email', (request, response) => deleteAccount(request, response, User));
 app.patch('/account/updateProfileData', (request, response) => updateProfileData(request, response, User));
-app.post("/account/uploadProfilePic", (request, response) => updateProfilePic(request, response, User));
+app.post('/account/uploadProfilePic', (request, response) => updateProfilePic(request, response, User));
 
 //users routes
-app.get("/users/search/:userName", (request, response) => searchForUser(request, response, User));
+app.get('/users/search/:userName', (request, response) => searchForUser(request, response, User));
 app.patch('/users/follow', (request, response) => follow(request, response, User));
+app.patch('/users/unFollow', (request, response) => unFollow(request, response, User));
 
 //socket route
 io.on('connection', socketConnection);
